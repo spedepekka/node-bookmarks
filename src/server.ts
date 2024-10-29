27881883b661741201183c9e15bf5a5fffd2b917
Jarno Tuovinen <spedepekka@gmail.com>
2024-10-29 19:51:16 +0200

    Authorization part 1

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
+import { protect } from './modules/auth'
 
 const app = express()
 
@@ -29,6 +30,6 @@ app.get('/', (req, res) => {
     res.status(200)
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
