81ba302b6d6f6c0c8a7d5b302354b107a40914ce
Jarno Tuovinen <spedepekka@gmail.com>
2024-10-30 08:04:14 +0200

    Add password compare and hashing

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,14 @@
 import jwt from "jsonwebtoken"
+import * as bcrypt from "bcrypt"
+
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash)
+}
+
+export const hashPassword = (password) => {
+  // TODO Generate salt
+  return bcrypt.hash(password, 5)
+}
 
 export const createJWT = (user) => {
   const token = jwt.sign(
