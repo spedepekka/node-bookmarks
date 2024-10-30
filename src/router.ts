f5e4b5002162f88b9247de408ca72988fe7f94d8
Jarno Tuovinen <spedepekka@gmail.com>
2024-10-30 09:54:22 +0200

    Use Express validator

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = Router()
 
@@ -28,7 +29,13 @@ router.get('/product', (req, res) => {
 
 router.get('/product/:id', (req, res) => {})
 
-router.put('/product/:id', (req, res) => {})
+router.put('/product/:id', body('name').isString(), (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.json({ message: 'PUT /product' })
+})
 
 router.post('/product', (req, res) => {})
 
